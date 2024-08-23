6dddf68a6e520b40e9eb084909e0634c570389cb
Xavier Felixan Mijaata <xavier357159@gmail.com>
2024-08-23 23:56:52 +0800

    Make banner

--- Frontend/app/components/Authentication/Authentication.js
+++ Frontend/app/components/Authentication/Authentication.js
@@ -2,12 +2,10 @@
 import { useState, useEffect } from "react";
 import Signup from "./Signup";
 import Login from "./Login";
-// import Header from "../Navbar/Header/Header"
 
 export default function Authentication({ setUserId }) {
   return (
     <div className="auth-container">
-      {/* <Header /> */}
       <Signup setUserId={setUserId} />
       <Login setUserId={setUserId} />
     </div>
--- Frontend/app/components/Navbar/Header/Header.js
+++ Frontend/app/components/Navbar/Header/Header.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import Image from 'next/image';
+import banner from "../../../assets/banner.png"
+
+const titleStyle = {
+  fontSize: '40px'
+};
 
 const Header = ({title}) => {
   return (
-    <div className="header-image">
-        <img src="./banner.png" alt="Banner" style={{ width: '100%' }} />
-        <div className="centered-title">{title}</div>
+    <div style={{ position: 'relative', width: '100%', height: '400px' }}>
+    <Image src={banner} alt='User Icon' style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+    <div
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, 50%)',
+        width: '50%',
+        height: '50%',
+        backgroundColor: 'rgba(255, 255, 255)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: '15px',
+      }}
+    >
+    <h1 style={titleStyle}>{title}</h1>
     </div>
+  </div>
   );
 };
 
