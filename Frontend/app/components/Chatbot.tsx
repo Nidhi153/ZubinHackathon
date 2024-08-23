6dbce95dd5843ea1817c72056df35fcdb0c7c89b
Jennifer Cheung <csjcheung@connect.ust.hk>
2024-08-23 12:21:13 +0800

    feat(chatbot): initial commit.

---
+++ Frontend/app/components/Chatbot.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Chatbot = () => {
+    return (<div>I am a chatbot</div>)
+}
+
+export default Chatbot
--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,7 +1,13 @@
+import Chatbot from "./components/chatbot"
+import styles from './global.module.scss'
+
 export default function Home() {
   return (
     <div>
       <a href="/authentication/signup">Sign up</a>
+      <div className={styles.chatbotWrapper}>
+        <Chatbot />
+      </div>
     </div>
   );
 }
