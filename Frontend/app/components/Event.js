687baf547c2061866da83a0c0abc0dff7f854470
MarcusCCH <marcuscch12@gmail.com>
2024-08-23 11:30:17 +0800

    feat(auth): add basic authentication

---
+++ Frontend/app/components/Event.js
@@ -0,0 +1,8 @@
+export default function Event({ userId }) {
+  return (
+    <div>
+      <h1>Event</h1>
+      <p>Welcome {userId}</p>
+    </div>
+  );
+}
--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+import { useEffect, useState } from "react";
+import Authentication from "./components/Authentication";
+import Event from "./components/Event";
+
 export default function Home() {
+  const [userId, setUserId] = useState(null);
+  const [showLogin, setShowLogin] = useState(true);
+
+  useEffect(() => {
+    console.log("userId", userId);
+    if (userId) {
+      setShowLogin(false);
+    }
+  }, [userId]);
   return (
     <div>
-      <a href="/authentication/signup">Sign up</a>
+      {showLogin && <Authentication setUserId={setUserId} />}
+
+      {userId && userId != "" && <Event userId={userId} />}
     </div>
   );
 }
