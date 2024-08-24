e55a8e4d93464266b536747b40367baa4f9b0cd5
MarcusCCH <marcuscch12@gmail.com>
2024-08-24 22:59:36 +0800

    feat: create qrcode

---
+++ qrcode/create_qrcode.py
@@ -0,0 +1,15 @@
+import qrcode
+import qrcode.image.svg
+from qrtools.qrtools import QR
+
+img = qrcode.make('123@hotmail.com', image_factory=qrcode.image.svg.SvgImage)
+
+with open('qr.svg', 'wb') as qr:
+    img.save(qr)
+
+# my_qr = QR(data=u"123@hotmail.com")
+# my_qr.encode()
+# my_qr.save('qr.svg')
+# qr = qrtools.QR()
+# qr.decode('qr.svg')
+
