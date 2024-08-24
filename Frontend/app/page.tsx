851dcd3ab5c218dc1d0a7e335c0959dcfe8c10cb
Jennifer Cheung <csjcheung@connect.ust.hk>
2024-08-24 23:58:26 +0800

    feat(style-homepage): styled homepage and related elements. Fixed incorrect styles.

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,13 +1,27 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import Event from "./components/AllEvent/AllEvent";
+import Image from "next/image"
+import backgroundImage from './assets/homepageImage.png'
+import styles from './app.module.scss'
+import EventCard from "./components/Event/EventCard/EventCard"
+import RegisteredEventCard from "./components/Event/RegisteredEventCard/RegisteredEventCard";
 
 export default function Home() {
-  useEffect(() => {}, []);
   return (
-    <div>
-      <Event />
+    <div className={styles.layout}>
+      <div className={styles.imageWrapper}>
+        <Image src={backgroundImage} alt="Homepage Image" />
+      </div>
+      <div className={styles.container}>
+        <div className={styles.header}>Upcoming Events</div>
+        <div className={styles.body}>
+          <div className={styles.cards}>
+            <EventCard title="Gathering Event" date="10/09/2024" description="Everyone can join!" />
+          </div>
+          <div className={styles.registeredEventsBox}>
+            <h1>Registered Events</h1>
+            <RegisteredEventCard title="Gathering Event" date="10/09/2024" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
