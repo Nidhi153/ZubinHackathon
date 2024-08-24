25353404e7b2243602b12ff28c4bf23e0d99e4b4
Xavier Felixan Mijaata <xavier357159@gmail.com>
2024-08-24 12:53:02 +0800

    Connect event page to API call

--- Frontend/app/components/AllEvent/AllEvent.js
+++ Frontend/app/components/AllEvent/AllEvent.js
@@ -2,6 +2,7 @@ import React from "react";
 import Box from '@mui/material/Box';
 
 import EventData from "../../data/event.json";
+import Event from "../Event/AllEvent"
 
 const buttonStyle = {
   backgroundColor: '#F0F0F0',
@@ -47,45 +48,19 @@ const arrowStyle = {
 
 
 
-const EventPage = () => {
+const EventPage = ({setUserId}) => {
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between', padding: '10px 10px', marginLeft: '5%', marginRight: '5%'}}>
       <div style={{ width: '60%', borderRadius: '5px', padding: '10px' }}>
-        {EventData.map((item, index) => (
-        <div style={buttonStyle} key={index}>
-          <Box
-            height={100}
-            width={800}
-            my={3}
-            display="flex"
-            alignItems="center"
-            justifyContent="space-between"
-            p={2}
-          >
-            <div>
-              <div className="text-center font-textfont font-bold" style={titleStyle}>
-                {item.EventName}
-              </div>
-              <div style={dateStyle}>
-                {item.Date}
-              </div>
-              <div style={dateStyle}>
-                {item.Subtitle}
-              </div>
-            </div>
-            <div style={arrowStyle}>{'>'}</div>
-          </Box>
-        </div>
-      ))}
+        <Event />
       </div>
-
-
-      
       <div style={{ width: '35%', border: '1px solid #ccc', borderRadius: '5px', padding: '10px' }}>
         <h2 style={registerStyle} className="text-center">Registered Events</h2>
-        {EventData.map((item, index) => (
+          {EventData.map((item, index) => (
           <div style={registeredEventStyle} key={index}>
             <Box
+            height={100}
+                    width={800}
               my={3}
               display="flex"
               alignItems="center"
@@ -104,6 +79,62 @@ const EventPage = () => {
           </div>
         ))}
       </div>
+
+    {/* // <div style={{ display: 'flex', justifyContent: 'space-between', padding: '10px 10px', marginLeft: '5%', marginRight: '5%'}}>
+    //   <div style={{ width: '60%', borderRadius: '5px', padding: '10px' }}>
+    //     {EventData.map((item, index) => ( */}
+    {/* //     <div style={buttonStyle} key={index}>
+    //       <Box
+    //         height={100}
+    //         width={800}
+    //         my={3}
+    //         display="flex"
+    //         alignItems="center"
+    //         justifyContent="space-between"
+    //         p={2}
+    //       > */}
+    {/* //         <div>
+    //           <div className="text-center font-textfont font-bold" style={titleStyle}>
+    //             {item.EventName}
+    //           </div>
+    //           <div style={dateStyle}>
+    //             {item.Date}
+    //           </div>
+    //           <div style={dateStyle}>
+    //             {item.Subtitle}
+    //           </div>
+    //         </div>
+    //         <div style={arrowStyle}>{'>'}</div>
+    //       </Box> */}
+    {/* //     </div> */}
+    {/* //   ))}
+    //   </div> */}
+
+
+      
+    {/* //   <div style={{ width: '35%', border: '1px solid #ccc', borderRadius: '5px', padding: '10px' }}>
+    //     <h2 style={registerStyle} className="text-center">Registered Events</h2>
+    //     {EventData.map((item, index) => ( */}
+    {/* //       <div style={registeredEventStyle} key={index}>
+    //         <Box */}
+    {/* //           my={3}
+    //           display="flex"
+    //           alignItems="center"
+    //           justifyContent="space-between"
+    //           px={2}
+    //         >
+    //           <div>
+    //             <div className="text-center font-textfont font-bold" style={titleStyle}>
+    //               {item.EventName}
+    //             </div>
+    //             <div style={dateStyle}>
+    //               {item.Date}
+    //             </div>
+    //           </div>
+    //         </Box> */}
+    {/* //       </div> */}
+    {/* //     ))}
+    //   </div> */}
     </div>
   );
 };
--- Frontend/app/components/Event/EventBox.js
+++ Frontend/app/components/Event/EventBox.js
@@ -1,14 +1,67 @@
 import { Card } from "flowbite-react";
 import "./Event.css";
+import Box from '@mui/material/Box';
+
+const buttonStyle = {
+  backgroundColor: '#F0F0F0',
+  border: '2px solid #000000',
+  borderRadius: '25px',
+  padding: '10px 20px',
+  fontSize: '16px',
+  cursor: 'pointer',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  margin: '10px'
+};
+
+const titleStyle = {
+  fontSize: '40px'
+};
+
+const dateStyle = {
+  fontSize: '14px'
+};
+
+const arrowStyle = {
+  fontSize: '30px'
+};
+
 export default function EventBox({ event }) {
   return (
-    <div className="event-box">
-      <Card>
-        <h2>{event.title}</h2>
-        <hr />
-        <p>{event.description}</p>
-        <p>Event date: {event.date}</p>
-      </Card>
-    </div>
+    <div style={buttonStyle} key={index}>
+    <Box
+      height={100}
+      width={800}
+      my={3}
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      p={2}
+    >
+      <div>
+        <div className="text-center font-textfont font-bold" style={titleStyle}>
+          {event.title}
+        </div>
+        <div style={dateStyle}>
+          {event.date}
+        </div>
+        <div style={dateStyle}>
+          {event.description}
+        </div>
+      </div>
+      <div style={arrowStyle}>{'>'}</div>
+    </Box>
+  </div>
+
+
+    // <div className="event-box">
+    //   <Card>
+    //     <h2>{event.title}</h2>
+    //     <hr />
+    //     <p>{event.description}</p>
+    //     <p>Event date: {event.date}</p>
+    //   </Card>
+    // </div>
   );
 }
