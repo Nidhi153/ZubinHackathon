4b10d64af6bb9c6def58c2b1177a97cdb9898abc
Xavier Felixan Mijaata <xavier357159@gmail.com>
2024-08-24 22:11:01 +0800

    Integrate member feedback page with API call

--- Frontend/app/components/Dashboard/CentralisedMessages.tsx
+++ Frontend/app/components/Dashboard/CentralisedMessages.tsx
@@ -1,12 +1,25 @@
 "use client";
 
+import styles from './EventTable.module.scss'
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableContainer,
+  } from '@chakra-ui/react'
+
 import { useRouter } from "next/navigation";
 import Button from "../components/Button/Button";
-import styles from "./account.module.scss";
+// import styles from "./account.module.scss";
 import BadgeRow from "./Row/BadgeRow";
 import TextRow from "./Row/TextRow";
 import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
+import MemberFeedback from "./MemberFeedback"
 const CentralisedMessage = () => {
   const [messages, setMessages] = useState([]);
   useEffect(() => {
@@ -38,7 +51,7 @@ const CentralisedMessage = () => {
 
   return (
     <div>
-      {messages.map((message) => (
+      {/* {messages.map((message) => (
         <div key={message._id}>
           <div>{message.message}</div>
           <div>{message.phonenumber}</div>
@@ -49,7 +62,50 @@ const CentralisedMessage = () => {
             <button>Submit</button>
           </form>
         </div>
-      ))}
+      ))} */}
+
+          <div className={styles.body}>
+            <div className={styles.heading}>Member Feedback</div>
+            <TableContainer className={styles.table}>
+                <Table variant='simple'>
+                    <Thead>
+                        <Tr>
+                            <Th>Index</Th>
+                            <Th>Message</Th>
+                            <Th>Phone Number</Th>
+                            <Th>Tag</Th>
+                            <Th>Reply</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {messages.map((message) => (
+                            <Tr key={message._id}>
+                                <Td className={styles.indexColumn}>{message.id}</Td>
+                                <Td className={`${styles.messageCell} ${styles.messageColumn}`}>{message.message}</Td>
+                                <Td className={styles.phoneColumn}>{message.phonenumber}</Td>
+                                <Td className={styles.tagColumn}>
+                                    {message.categories && message.categories.map((category, index) => (
+                                        <span key={index}>
+                                            {category}
+                                            <br />
+                                        </span>
+                                    ))}
+                                </Td>
+                                <Td className={styles.replyColumn}>
+                                <form onSubmit={(e) => submitResponse(message.phonenumber, e)}>
+                                  <input type="text" placeholder="Response" name="message" style={{ width: '400px' }}></input>
+                                  <br />
+                                  <button className={styles.buttonWrapper}>Submit</button>
+                                </form>
+                                </Td>
+                            </Tr>
+                        ))}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </div>
+
+      {/* <MemberFeedback messages={messages}/> */}
     </div>
   );
 };
