d66819eda8fcabbe1af963b35de2cfa74080355d
Xavier Felixan Mijaata <xavier357159@gmail.com>
2024-08-26 01:32:02 +0800

    Add header to past events

--- Frontend/app/components/PastEvent/PastEvent.js
+++ Frontend/app/components/PastEvent/PastEvent.js
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import styles from './eventDetails.module.scss'
 // import Box from '@mui/material/Box';
 
 import Event from "../Event/AllEvent";
@@ -12,16 +13,21 @@ const PastEventPage = () => {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        padding: "10px 10px",
-        marginLeft: "25%",
-        marginRight: "25%",
-      }}
-    >
-      <Event filterFunction={filterPastEvents} />
+    <div>
+        <div className={styles.body}>
+            <div className={styles.heading}>Member Feedback</div>
+            <div
+            style={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: "10px 10px",
+                marginLeft: "25%",
+                marginRight: "25%",
+            }}
+            >
+            <Event filterFunction={filterPastEvents} />
+            </div>
+        </div>
     </div>
   );
 };
