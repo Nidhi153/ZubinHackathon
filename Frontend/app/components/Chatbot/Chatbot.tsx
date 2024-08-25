06b04526a80363ae72cbdc4880b8ce31241375fc
MarcusCCH <marcuscch12@gmail.com>
2024-08-25 14:29:55 +0800

    feat: add button on hover show the chatbot

--- Frontend/app/components/Chatbot/Chatbot.tsx
+++ Frontend/app/components/Chatbot/Chatbot.tsx
@@ -1,26 +1,48 @@
-import styles from './Chatbot.module.scss'
-import sendIcon from '../../assets/sendIcon.svg'
-import Image from 'next/image'
-import Message from './Message/Message'
-
-/* Messages are currently only placeholders. Edit this file for receiving messages. */
-const Chatbot = () => {
-    return (
-        <div className={styles.chatbot}>
-            <div className={styles.header}>AI Chatbot</div>
-            <div className={styles.body}>
-                <div className={styles.messages}>
-                    <Message text='This is a very long message.' from='bot' />
-                    <Message text='This is a very long message.' from='user' />
-                    <Message text='Short message.' from='bot' />
-                </div>
-                <div className={styles.inputBox}>
-                    <input className={styles.input} placeholder='Enter your message...' />
-                    <Image src={sendIcon} alt='Send Icon' />
-                </div>
-            </div>
+"use client";
+import styles from "./Chatbot.module.scss";
+import sendIcon from "../../assets/sendIcon.svg";
+import Image from "next/image";
+import Message from "./Message/Message";
+import { useState } from "react";
+const Chatbot = ({ setIsChatbotVisible }) => {
+  return (
+    <div
+      className={styles.chatbot}
+      onMouseLeave={() => setIsChatbotVisible(false)}
+    >
+      <div className={styles.header}>AI Chatbot</div>
+      <div className={styles.body}>
+        <div className={styles.messages}>
+          <Message text="This is a very long message." from="bot" />
+          <Message text="This is a very long message." from="user" />
+          <Message text="Short message." from="bot" />
         </div>
-    )
-}
+        <div className={styles.inputBox}>
+          <input className={styles.input} placeholder="Enter your message..." />
+          <Image src={sendIcon} alt="Send Icon" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chatbot
+const ChatbotContainer = () => {
+  const [isChatbotVisible, setIsChatbotVisible] = useState(false);
+
+  return (
+    <div className={styles.chatbotContainer}>
+      {isChatbotVisible ? (
+        <Chatbot setIsChatbotVisible={setIsChatbotVisible} />
+      ) : (
+        <button
+          onMouseEnter={() => setIsChatbotVisible(true)}
+          className="chatbotButton"
+        >
+          🤖 Chat
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ChatbotContainer;
