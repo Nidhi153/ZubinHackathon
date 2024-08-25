d8ed463e959762e7c0e72e36c060e577332e6f5c
Syed Nafis Ishrak <ishrak@connect.hku.hk>
2024-08-25 15:24:19 +0800

    added whatsapp api integration

--- PythonAi/main.py
+++ PythonAi/main.py
@@ -11,18 +11,28 @@ from whatsapp_api.relay import get_text_message_input, get_image_input, send_mes
 load_dotenv()
 HF_TOKEN = os.getenv("HF_TOKEN")
 
+# Initialize classes
 app = FastAPI()
-
 chat = Chatbot()
-
 client = init_chatbot(HF_TOKEN)
+
 class Broadcast(BaseModel):
     phonenumbers: list
     broadcastmessage: str
 
 class Image(BaseModel):
     phonenumbers: list
-    image_id: str
+    imageid: str
+    caption: str
+
+@app.get("/hello")
+async def hello():
+    return {"message": "Hello, World!"}
+
+
+@app.get("/helloworld")
+async def hello_world():
+    return {"message": "Hello, World!"}
 
 @app.post("/ai/recommendation")
 async def get_recommended_events(request: Request):
@@ -30,54 +40,10 @@ async def get_recommended_events(request: Request):
     response = recommendation.recommend_events(request_data)
     return JSONResponse(content=response)
 
-
 @app.post("/ai/chatbot")
 async def get_chat_response(request: Request):
-
-    # Parse the JSON request body
     request_data = await request.json()
-
     response = chat.query(request_data, client)
-
-    # Return the response as a JSON response
-    return JSONResponse(content=response)
-
-
-@app.get("/hello")
-async def hello():
-    return {"message": "Hello, World!"}
-
-    # Return the response as a JSON response
-
-
-@app.get("/helloworld")
-async def hello_world():
-    return {"message": "Hello, World!"}  # Return a JSON response
-  
-
-class Broadcast(BaseModel):
-    phonenumbers: list
-    broadcastmessage: str
-
-class Image(BaseModel):
-    phonenumbers: list
-    image_id: str
-
-@app.post("/recommendation")
-async def get_recommended_events(request: Request):
-    request_data = await request.json()
-    response = recommendation.recommend_events(request_data)
-    return JSONResponse(content=response)
-
-@app.post("/ai/chatbot")
-async def get_chat_response(request: Request):
-
-    # Parse the JSON request body
-    request_data = await request.json()
-
-    response = chat.query(request_data, client)
-
-    # Return the response as a JSON response
     return JSONResponse(content=response)
 
 @app.post("/ai/whatsapp/broadcast")
@@ -86,7 +52,8 @@ def relay(item: Broadcast):
         send_message(get_text_message_input(phone_num, item.broadcastmessage))
     return item
 
+
 @app.post("/ai/whatsapp/images")
 def relay_images(item: Image):
     for phone_num in item.phonenumbers:
-        send_message(get_image_input(phone_num, item.image_id))3
+        send_message(get_image_input(phone_num, item.imageid, item.caption))
--- PythonAi/whatsapp_api/relay.py
+++ PythonAi/whatsapp_api/relay.py
@@ -1,36 +1,36 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import os, requests, json
+from dotenv import load_dotenv
 
-#Run: fastapi dev relay.py
+# Get tokens
+load_dotenv()
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+APP_ID = os.getenv("APP_ID")
+APP_SECRET = os.getenv("APP_SECRET")
+VERSION = os.getenv("VERSION")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
 
-"""
-Constants
-"""
+# app = FastAPI()
 
-#Please update API Token daily!
-ACCESS_TOKEN = "EABycHlgN6cgBOxh3AL5VnwML8PxXuS6821KKoUqR2ZAvJ77UwpMZBVGGhROZBvR4obtDuouZBB5iBGEKkHxnqTfZBqtyj8N7QfWy1yk08GgevnBfCW62v2ZAzU6lPZCkEg12dy0ZASZCXGKQnKZCLSb5HETVmR7OIcBZBcMlO6SgZCMvdYkvlLws29p5JuLBs87nWKnIrfhGcJ5XhBVzOQjJV0IZD"
+# class Broadcast(BaseModel):
+#     phonenumbers: list
+#     broadcastmessage: str
 
-APP_ID = "8052953488157128"
+# class Image(BaseModel):
+#     phonenumbers: list
+#     image_id: str
 
-APP_SECRET = "adce737c3c4faa7191aedf206448a7a4"
+# @app.post("/ai/whatsapp/broadcast")
+# def relay(item: Broadcast):
+#     for phone_num in item.phonenumbers:
+#         send_message(get_text_message_input(phone_num, item.broadcastmessage))
+#     return item
 
-VERSION="v20.0"
-
-PHONE_NUMBER_ID="423268137527656"
-
-app = FastAPI()
-
-class Broadcast(BaseModel):
-    phonenumbers: list
-    broadcastmessage: str
-
-@app.post("/ai/whatsapp/broadcast")
-def relay(item: Broadcast):
-    for phone_num in item.phonenumbers:
-        send_message(get_text_message_input(phone_num, item.broadcastmessage))
-
-    return item
+# @app.post("/ai/whatsapp/images")
+# def relay_images(item: Image):
+#     for phone_num in item.phonenumbers:
+#         send_message(get_image_input(phone_num, item.image_id))
 
 def get_text_message_input(recipient, text):
     return json.dumps(
@@ -39,19 +39,20 @@ def get_text_message_input(recipient, text):
             "recipient_type": "individual",
             "to": recipient,
             "type": "text",
-            "text": {"preview_url": False, "body": text}
+            "text": {"preview_url": False, "body": text},
         }
     )
 
 def send_message(data):
     headers = {
         "Content-type": "application/json",
-        "Authorization": f"Bearer {ACCESS_TOKEN}"
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
     }
 
     url = f"https://graph.facebook.com/{str.lower(VERSION)}/{PHONE_NUMBER_ID}/messages"
 
     response = requests.post(url, data=data, headers=headers)
+
     if response.status_code == 200:
         print("Status:", response.status_code)
         print("Content-type:", response.headers["content-type"])
@@ -62,3 +63,13 @@ def send_message(data):
         print(response.text)
         return response
 
+def get_image_input(recipient, image_id: str, caption: str = ""):
+    return json.dumps(
+        {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": recipient,
+            "type": "image",
+            "image": {"id": image_id, "caption": caption},
+        }
+    )
