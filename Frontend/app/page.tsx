a46f971fcc2b673a145ec4a138c02740897b362f
Jennifer Cheung <csjcheung@connect.ust.hk>
2024-08-25 13:39:49 +0800

    feat(style-homepage): Changed page styles for volunteer.

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,10 +1,23 @@
+'use client'
+
 import Image from "next/image"
 import backgroundImage from './assets/homepageImage.png'
 import styles from './app.module.scss'
 import EventCard from "./components/Event/EventCard/EventCard"
-import RegisteredEventCard from "./components/Event/RegisteredEventCard/RegisteredEventCard";
+import RegisteredEventCard from "./components/Event/RegisteredEventCard/RegisteredEventCard"
+import Cookies from "js-cookie"
+import { useEffect, useState } from "react"
 
 export default function Home() {
+  const [role, setRole] = useState<string>('')
+
+  useEffect(() => {
+    const role = Cookies.get('role')
+    if (role) {
+      setRole(role)
+    }
+  }, [])
+
   return (
     <div className={styles.layout}>
       <div className={styles.imageWrapper}>
@@ -14,7 +27,21 @@ export default function Home() {
         <div className={styles.header}>Upcoming Events</div>
         <div className={styles.body}>
           <div className={styles.cards}>
-            <EventCard title="Gathering Event" date="10/09/2024" description="Everyone can join!" />
+            {role === 'volunteer'
+              ? <div className={styles.eventCardSection}>
+                <h1>Suggested Events</h1>
+                <div className={styles.eventCards}>
+                  <EventCard title="Gathering Event" date="10/09/2024" description="Everyone can join!" />
+                </div>
+              </div>
+              : ''}
+
+            <div className={styles.eventCardSection}>
+              {role === 'volunteer' ? <h1>Other Events</h1> : ''}
+              <div className={styles.eventCards}>
+                <EventCard title="Gathering Event" date="10/09/2024" description="Everyone can join!" />
+              </div>
+            </div>
           </div>
           <div className={styles.registeredEventsBox}>
             <h1>Registered Events</h1>
