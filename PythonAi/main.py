b0c89a0a54d60b700a47494852f8735a18921038
Syed Nafis Ishrak <ishrak@connect.hku.hk>
2024-08-25 14:25:13 +0800

    added whatsapp api routing

--- PythonAi/main.py
+++ PythonAi/main.py
@@ -5,6 +5,8 @@ from ai_chatbot import Chatbot
 from ai_chatbot import init_chatbot
 import recommendation
 from dotenv import load_dotenv
+from pydantic import BaseModel
+from whatsapp_api.relay import get_text_message_input, get_image_input, send_message
 
 load_dotenv()
 HF_TOKEN = os.getenv("HF_TOKEN")
@@ -14,7 +16,13 @@ app = FastAPI()
 chat = Chatbot()
 
 client = init_chatbot(HF_TOKEN)
+class Broadcast(BaseModel):
+    phonenumbers: list
+    broadcastmessage: str
 
+class Image(BaseModel):
+    phonenumbers: list
+    image_id: str
 
 @app.post("/ai/recommendation")
 async def get_recommended_events(request: Request):
@@ -45,3 +53,40 @@ async def hello():
 @app.get("/helloworld")
 async def hello_world():
     return {"message": "Hello, World!"}  # Return a JSON response
+  
+
+class Broadcast(BaseModel):
+    phonenumbers: list
+    broadcastmessage: str
+
+class Image(BaseModel):
+    phonenumbers: list
+    image_id: str
+
+@app.post("/recommendation")
+async def get_recommended_events(request: Request):
+    request_data = await request.json()
+    response = recommendation.recommend_events(request_data)
+    return JSONResponse(content=response)
+
+@app.post("/ai/chatbot")
+async def get_chat_response(request: Request):
+
+    # Parse the JSON request body
+    request_data = await request.json()
+
+    response = chat.query(request_data, client)
+
+    # Return the response as a JSON response
+    return JSONResponse(content=response)
+
+@app.post("/ai/whatsapp/broadcast")
+def relay(item: Broadcast):
+    for phone_num in item.phonenumbers:
+        send_message(get_text_message_input(phone_num, item.broadcastmessage))
+    return item
+
+@app.post("/ai/whatsapp/images")
+def relay_images(item: Image):
+    for phone_num in item.phonenumbers:
+        send_message(get_image_input(phone_num, item.image_id))3
