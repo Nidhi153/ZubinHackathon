f228c4872aa61359eb479c427fa291ba7de59eea
Jennifer Cheung <csjcheung@connect.ust.hk>
2024-08-26 09:05:32 +0800

    chore: cleaned up frontend homepage code

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,28 +1,12 @@
 "use client";
 
-import styles from "./app.module.scss";
-import Cookies from "js-cookie";
-import { useEffect, useState } from "react";
-import HomepageLayout from "./components/HomepageLayout/HomepageLayout";
-import RegisteredEventBox from "./components/Event/RegisteredEventBox/RegisteredEventBox";
-import EventCardGroup from "./components/Event/EventCardGroup/EventCardGroup";
 import Event from "./components/AllEvent/AllEvent";
+import HomepageLayout from "./components/HomepageLayout/HomepageLayout";
+
 export default function Home() {
-  const [role, setRole] = useState<string>("");
-
-  useEffect(() => {
-    const role = Cookies.get("role");
-    if (role) {
-      setRole(role);
-    }
-  }, []);
-
   return (
     <HomepageLayout header="Upcoming Events">
-      {/* <div className={styles.body}> */}
-      {/* <div className={styles.cards}></div> */}
       <Event />
-      {/* </div> */}
     </HomepageLayout>
   );
 }
